0afff0be0283249d7177c356b590fa8dcbb63efa
rtelicak <roman.telicak@gmail.com>
2014-03-24 23:18:26 +0100

    remove unused php scripts

--- web-app/application/logs/log-2014-03-24.php
+++ web-app/application/logs/log-2014-03-24.php
@@ -2397,3 +2397,58 @@ DEBUG - 2014-03-24 22:33:26 --> Final output sent to browser
 DEBUG - 2014-03-24 22:33:26 --> Total execution time: 0.0127
 DEBUG - 2014-03-24 22:33:26 --> Final output sent to browser
 DEBUG - 2014-03-24 22:33:26 --> Total execution time: 0.0314
+DEBUG - 2014-03-24 23:16:16 --> Config Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Hooks Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Utf8 Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> UTF-8 Support Enabled
+DEBUG - 2014-03-24 23:16:16 --> URI Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Router Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> No URI present. Default controller set.
+DEBUG - 2014-03-24 23:16:16 --> Output Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Security Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Input Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-03-24 23:16:16 --> Language Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Loader Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Helper loaded: url_helper
+DEBUG - 2014-03-24 23:16:16 --> Database Driver Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Session Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Helper loaded: string_helper
+DEBUG - 2014-03-24 23:16:16 --> Session routines successfully run
+DEBUG - 2014-03-24 23:16:16 --> Controller Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Model Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Model Class Initialized
+DEBUG - 2014-03-24 23:16:16 --> Helper loaded: input_generator_helper
+ERROR - 2014-03-24 23:16:16 --> Severity: Warning  --> include(includes/head_frontend.php): failed to open stream: No such file or directory /Users/rmn/Developer/DP/web-app/application/views/home.php 1
+ERROR - 2014-03-24 23:16:16 --> Severity: Warning  --> include(): Failed opening 'includes/head_frontend.php' for inclusion (include_path='.:/Applications/MAMP/bin/php/php5.4.4/lib/php') /Users/rmn/Developer/DP/web-app/application/views/home.php 1
+ERROR - 2014-03-24 23:16:16 --> Severity: Warning  --> include(includes/menu_frontend.php): failed to open stream: No such file or directory /Users/rmn/Developer/DP/web-app/application/views/home.php 6
+ERROR - 2014-03-24 23:16:16 --> Severity: Warning  --> include(): Failed opening 'includes/menu_frontend.php' for inclusion (include_path='.:/Applications/MAMP/bin/php/php5.4.4/lib/php') /Users/rmn/Developer/DP/web-app/application/views/home.php 6
+ERROR - 2014-03-24 23:16:16 --> Severity: Warning  --> include(includes/footer_frontend.php): failed to open stream: No such file or directory /Users/rmn/Developer/DP/web-app/application/views/home.php 330
+ERROR - 2014-03-24 23:16:16 --> Severity: Warning  --> include(): Failed opening 'includes/footer_frontend.php' for inclusion (include_path='.:/Applications/MAMP/bin/php/php5.4.4/lib/php') /Users/rmn/Developer/DP/web-app/application/views/home.php 330
+DEBUG - 2014-03-24 23:16:16 --> File loaded: application/views/home.php
+DEBUG - 2014-03-24 23:16:16 --> Final output sent to browser
+DEBUG - 2014-03-24 23:16:16 --> Total execution time: 0.4344
+DEBUG - 2014-03-24 23:17:17 --> Config Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Hooks Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Utf8 Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> UTF-8 Support Enabled
+DEBUG - 2014-03-24 23:17:17 --> URI Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Router Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> No URI present. Default controller set.
+DEBUG - 2014-03-24 23:17:17 --> Output Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Security Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Input Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-03-24 23:17:17 --> Language Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Loader Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Helper loaded: url_helper
+DEBUG - 2014-03-24 23:17:17 --> Database Driver Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Session Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Helper loaded: string_helper
+DEBUG - 2014-03-24 23:17:17 --> Session routines successfully run
+DEBUG - 2014-03-24 23:17:17 --> Controller Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Model Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> Model Class Initialized
+DEBUG - 2014-03-24 23:17:17 --> File loaded: application/views/home.php
+DEBUG - 2014-03-24 23:17:17 --> Final output sent to browser
+DEBUG - 2014-03-24 23:17:17 --> Total execution time: 0.1262
